a7eb74e3322995a53c15cc527fc69ae99effd09f
ShadowHatesYou <31442897+ShadowHatesYou@users.noreply.github.com>
2019-04-18 06:27:24 -0400

    Initial commit

---
+++ pipetap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Written by Shadow
+TCPDUMPFILTER="ip host not $(dig +short myip.opendns.com @resolver1.opendns.com) and not tcp port 22"
+
+if [ $# != 2 ]; then
+        echo -e "This script invokes tcpdump over ssh piping to STDOUT, allowing remote use of wireshark"
+        echo -e "Script invocation: $0 user@<ip> <interface|ANY>"
+	echo -e "TCPDUMPFILTER variable must be set in the script to avoid exponential traffic over ssh"
+        exit
+fi
+if [ -z "$TCPDUMPFILTER" ]; then
+        TCPDUMPFILTER="ip host not $(dig +short myip.opendns.com @resolver1.opendns.com) and not tcp port 22"
+fi
+ssh -A -C $1 tcpdump -s 0 -i $2 -w - $TCPDUMPFILTER | sudo wireshark -k -i -
+
